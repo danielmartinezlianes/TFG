15c0cc0f2e578d1cfb31c8b847dc65309832c7ec
danielmartinezlianes <danielmartinezlianes@hotmail.com>
2023-08-28 16:30:56 +0200

    ULTIMA LINEA

--- TFG/TFG.py
+++ TFG/TFG.py
@@ -7,6 +7,7 @@ def Main():
     print("Introduce la ruta del fichero a traducir: ")
     f_entrada= input()
     TraduceInstancias(f_salida,f_entrada)
+    TraduceHechos(f_salida,f_entrada)
     TraduceReglas(f_salida,f_entrada)
     print('El fichero se ha generado en la ruta '+f_salida)
 
@@ -17,20 +18,10 @@ def CreaClases(f_salida):
 	    (slot Nombre (type SYMBOL))
     )
 
-(defclass tablero
-	    (is-a USER)
-        (slot Nombre (type SYMBOL))
-	    (slot x (type INTEGER))
-	    (slot y (type INTEGER))
-	    (slot estado (type SYMBOL))
-    )
-
 (defclass turno
 	    (is-a USER)
 	    (slot Nombre (type SYMBOL))
     )
-
-
     """)
     f.close
 
@@ -46,10 +37,6 @@ def TraduceInstancias(f_salida,f_entrada):
                 {
                     f.write("   (of turno (Nombre " + word[2].replace(')','')+"))\n")
                 }
-            elif ('(init' in word and '(control' not in word and len(word)>=5):
-                {
-                f.write("   (of tablero (Nombre "+word[1].replace('(','') +")"+ "(x "+word[2]+")"+"(y "+word[3]+")"+"(estado "+word[4].replace(')','')+"))"+"\n")
-                }
             if ('(role' in word ):
                 {
                     f.write("   (of jugador (Nombre " + word[1].replace(')','')+"))\n")
@@ -57,6 +44,21 @@ def TraduceInstancias(f_salida,f_entrada):
             line = file.readline();
         f.write(")");
         f.close
+
+def TraduceHechos(f_salida,f_entrada):
+    with open( f_entrada, 'r') as file:
+        f = open(f_salida+"\\resultado.clp", "a")
+        line = file.readline()
+        f.write("\n(deffacts hechos-iniciales \n");
+        while line:
+            
+            word=str(line).split()
+            if ('(init' in word and '(control' not in word and len(word)>=5):
+                f.write(line.replace('(init ','').replace(')','').replace('\n','')+")\n")
+            line = file.readline();
+        f.write(")");
+        f.close
+
 
 def TraduceReglas(f_salida,f_entrada):
     with open( f_entrada, 'r') as file:
@@ -66,6 +68,7 @@ def TraduceReglas(f_salida,f_entrada):
         array=list()
         acciones=1
         goal=1
+        abecedario_minusculas = list(string.ascii_lowercase)
         while line:
             does=0
             word=str(line).split()
@@ -73,13 +76,14 @@ def TraduceReglas(f_salida,f_entrada):
                 array.insert(0,line)
                 line = file.readline();
                 word=str(line).split()
+                condiciones=""
                 if('(control' not in word):
                     condicion=2
                     while(line!="\n"):
                         if('(does' in word):
                             array.insert(1,word[1])
                             does=1
-                        elif('(true' in word):
+                        else:
                             array.insert(condicion,line)
                             condicion+=1
                         line = file.readline();
@@ -89,16 +93,36 @@ def TraduceReglas(f_salida,f_entrada):
                     if(does==1):
                         f.write("?d <- (object (is-a jugador) (Nombre "+array[1]+"))\n")
                         while(contador>1):
-                                word=str(array[contador]).split()
-                                f.write(" ?ag <- (object (is-a tablero) (Nombre "+word[1].replace('(','')+")(x "+word[2]+")(y "+word[3]+")(estado "+word[4].replace(')','')+"))\n")
+                            word=str(array[contador]).split()
+                            if('(not' not in word):
+                                word='?'+abecedario_minusculas[contador]+' <- '+str(array[contador])+''
+                                condiciones=condiciones+word.replace('(true ','').replace(')','').replace('\n','')+")\n"
+                                f.write(condiciones.replace('(or',''))
                                 contador=contador-1
+                                line = file.readline();
+                            else:
+                                word=str(array[contador])
+                                condiciones=condiciones+word.replace('(true ','').replace(')','').replace('\n','')+"))\n"
+                                f.write(condiciones.replace('(or',''))
+                                contador=contador-1
+                                line = file.readline();
                     else:
                         while(contador>=1):
-                                word=str(array[contador]).split()
-                                f.write(" ?ag <- (object (is-a tablero) (Nombre "+word[1].replace('(','')+")(x "+word[2]+")(y "+word[3]+")(estado "+word[4].replace(')','')+"))\n")
+                            word=str(array[contador]).split()
+                            if('(not' not in word):
+                                word='?'+abecedario_minusculas[contador]+' <- '+str(array[contador])+''
+                                condiciones=condiciones+word.replace('(true ','').replace(')','').replace('\n','')+")\n"
+                                f.write(condiciones.replace('(or',''))
                                 contador=contador-1
-                    word=str(array[0]).split()
-                    f.write("=>\n(modify ?ag (Nombre "+word[2].replace('(','')+")(x "+word[3]+")(y "+word[4]+")(estado "+word[len(word)-1].replace(')','')+")))\n")
+                                line = file.readline();
+                            else:
+                                word=str(array[contador])
+                                condiciones=condiciones+word.replace('(true ','').replace(')','').replace('\n','')+"))\n"
+                                f.write(condiciones.replace('(or',''))
+                                contador=contador-1
+                                line = file.readline();
+                    word=str(array[0])
+                    f.write("=>\n(assert "+word.replace('(<= (next','').replace(')','').replace('\n','')+")))\n")
                     acciones+=1
                     array.clear()
                 else:
@@ -107,9 +131,6 @@ def TraduceReglas(f_salida,f_entrada):
             elif('(<=' in word and '(goal' not in word and 'terminal' not in word and '(legal' not in word):
                 fact=line
                 line = file.readline();
-                abecedario_minusculas = list(string.ascii_lowercase)
-                
-                
                 contador=0
                 condiciones=""
                 while(line!="\n" and line!=")\n" and len(line) != 0):
@@ -118,9 +139,14 @@ def TraduceReglas(f_salida,f_entrada):
                     if('(true' in word):
                         line='?'+abecedario_minusculas[contador]+' <- '+line+''
                         condiciones=condiciones+line.replace('(true ','').replace(')','').replace('\n','')+")\n"
-                    
+                    elif('(not' not in word):
+                        line='?'+abecedario_minusculas[contador]+' <- '+line+''
+                        condiciones=condiciones+line.replace(')','').replace('\n','')+")\n"
+                    else:
+                       condiciones=condiciones+line.replace(')','').replace('\n','')+"))\n"
                     line = file.readline();
                 if(condiciones!=""):
+                    condiciones=condiciones.replace('(or','')
                     f.write("\n(defrule accion"+str(acciones)+"\n")
                     f.write(condiciones)
                     f.write("=>\n(assert "+fact.replace('(<= ','')+"))\n")
@@ -150,11 +176,3 @@ def TraduceReglas(f_salida,f_entrada):
 
 Main()
 
-
-
-
-#f.write("\n(defrule inicio\n ?d <- (object (is-a jugador) (Nombre "+word[2]+"))\n")
-                    #line = file.readline();
-                    #word2=str(line).split()
-                    #f.write(" ?ag <- (object (is-a tablero) (Nombre "+word2[1].replace('(','')+")(x ?x)(y ?y)(estado "+word2[len(word2)-1].replace(')','')+"))\n=>\n(modify ?ag (estado "+word[2]+")))\n")
-              #line = file.readline();    
